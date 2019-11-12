74cff251f3c26b43ac2840759b9f68a91c1f2fab
Kulin Sitwala <kulinsitwala@Kulins-MacBook-Pro-2.local>
2019-11-12 20:43:16 +0530

    Fixed the failure of cast and added failure reasons

--- quantum-starter/src/main/java/com/quantum/tests/calculator/AddTests.java
+++ quantum-starter/src/main/java/com/quantum/tests/calculator/AddTests.java
@@ -4,25 +4,22 @@ import java.util.Map;
 
 import org.testng.annotations.Test;
 
-import com.qmetry.qaf.automation.core.TestBaseProvider;
-import com.qmetry.qaf.automation.data.MetaData;
 import com.qmetry.qaf.automation.testng.dataprovider.QAFDataProvider;
 import com.qmetry.qaf.automation.ui.WebDriverTestCase;
 import com.quantum.pages.CalculatorPage;
-import com.quantum.utils.DeviceUtils;
-import com.quantum.utils.DriverUtils;
 
 public class AddTests extends WebDriverTestCase {
 
+	@SuppressWarnings("unlikely-arg-type")
 	@QAFDataProvider(dataFile = "src/main/resources/data/testData.csv")
 	@Test(groups = { "regression" })
-	public void addTest(Map<String, String> data) {
+	public void addTest(Map<Integer, Integer> data) {
 		CalculatorPage cp = new CalculatorPage();
 
 		cp.startApp("Calculator");
 		cp.clearCalculator();
-		cp.addInto(Integer.parseInt(data.get("int1")), Integer.parseInt(data.get("int2")));
-		cp.resultShouldBe(Integer.parseInt(data.get("result")));
+		cp.addInto(data.get("int1"), data.get("int2"));
+		cp.resultShouldBe(data.get("result"));
 	}
 
 }
